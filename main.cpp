86ac627ec97c3e26caf27d197d3f6a6bbaa6f778
BSMcGee14 <bmcgee6@mail.csuchio.edu>
2018-09-19 16:59:21 -0700

    LazyHoroscope

--- main.cpp
+++ main.cpp
@@ -1,16 +1,27 @@
-//Authors:
+//Authors: Brandan McGee
 #include<iostream>
 #include<string>
 
 using namespace std;
 
+
 int main()
 {
   //declare variables
+  string firstName, lastName;
+  int lucky;
 
-  //get user input
+  //get user input 
+  cout << "Please enter your first name: ";
+  cin >> firstName;
+  cout << "Please enter your last name: ";
+  cin >> lastName;
+  cout << "Welcome " << firstName[0]<< "." << lastName[0] << " here is your fortune..." << endl;
+  
 
   //tell fortune
+  lucky = firstName.length();
+  cout << "your lucky number is... " << lucky << endl;
 
   return 0;
 }
