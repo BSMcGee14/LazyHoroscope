7f8e228f1a1a7451ca212fb43328f29f0ad0ab3d
BSMcGee14 <bmcgee6@mail.csuchio.edu>
2018-09-24 16:48:45 -0700

    LazyHoroscope Part 2

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ int main()
   //declare variables
   string firstName, lastName;
   int lucky;
+  bool vowel = false;
 
   //get user input 
   cout << "Please enter your first name: ";
@@ -17,11 +18,20 @@ int main()
   cout << "Please enter your last name: ";
   cin >> lastName;
   cout << "Welcome " << firstName[0]<< "." << lastName[0] << " here is your fortune..." << endl;
-  
+  if (firstName[0] == 'a' || firstName[0] == 'e' || firstName[0] == 'i' || firstName[0] == 'o' || firstName[0] == 'u' || firstName[0] == 'A' || firstName[0] == 'E' || firstName[0] == 'I' || firstName[0] == 'O' || firstName[0] == 'U')
+  vowel = true;
+  if (vowel == true)
+  {
+     cout << "You're destined to be famous! You have aleady met your true!" << endl;
+  }
+  else
+  {
+    cout << "...you should keep a low profile..." << endl;
+  }
 
   //tell fortune
   lucky = firstName.length();
-  cout << "your lucky number is... " << lucky << endl;
+  cout << "Your lucky number is... " << lucky << endl;
 
   return 0;
 }
