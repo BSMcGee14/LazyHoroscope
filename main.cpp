c861d401583ff71e9611590e13a9b0d013cb91a2
BSMcGee14 <bmcgee6@mail.csuchio.edu>
2018-09-24 22:58:15 -0700

    LazyHoroscope Part 2

--- main.cpp
+++ main.cpp
@@ -13,18 +13,20 @@ int main()
   bool vowel = false;
 
   //get user input 
-  cout << "What is  your first name?" << endl;
+  cout << "What is your first name?" << endl;
   cin >> firstName;
   cout << "What is your last name?" << endl;
   cin >> lastName;
   cout << "Welcome, " << firstName[0]<< "." << lastName[0] << "."  << ", here is your fortune..." << endl;
   lucky = firstName.length();
-  cout << "your lucky number is  " << lucky << endl;
+  cout << "your lucky number is " << lucky << endl;
   if (firstName[0] == 'a' || firstName[0] == 'e' || firstName[0] == 'i' || firstName[0] == 'o' || firstName[0] == 'u' || firstName[0] == 'A' || firstName[0] == 'E' || firstName[0] == 'I' || firstName[0] == 'O' || firstName[0] == 'U')
   vowel = true;
   if (vowel == true)
   {
-     cout << "You're destined to be famous! You have aleady met your true!" << endl;
+     cout << "you are destined to be famous!" << endl;
+     cout << "you have aleady met your true!" << endl;
+     cout << "have a good day!" << endl;
   }
   else
   {
